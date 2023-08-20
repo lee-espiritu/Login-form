4afe0c5f6d44d901b2de9833ae9bda8a9a407933
lee-espiritu <130622275+lee-espiritu@users.noreply.github.com>
2023-08-20 12:22:14 +1000

    Password max length counter HTML JS CSS

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 //===================================== USERNAME COUNTER =========================
 let username_form = document.getElementById("username");
-let counter = document.getElementById("username-counter");
-let userMaxLength = username_form.getAttribute("maxlength");
+let username_counter = document.getElementById("username-counter");
+let username_max_length = username_form.getAttribute("maxlength");
 username_form.addEventListener('input', function(event){
-    counter.innerText = userMaxLength - username_form.value.length;
+    username_counter.innerText = username_max_length - username_form.value.length;
+});
+
+//===================================== USERNAME COUNTER =========================
+let password_form = document.getElementById("password");
+let password_counter = document.getElementById("password-counter");
+let password_max_length = password_form.getAttribute("maxlength");
+password_form.addEventListener('input', function(event){
+    password_counter.innerText = password_max_length - password_form.value.length;
 });
